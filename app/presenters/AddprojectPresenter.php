df0c6a3860b4b1c3d1bf9b5702b593e367681dc3
Dominik <dominikslechta.1@gmail.com>
2018-12-18 08:10:53 +0100

    project add/edit/delete/public/locked

--- app/presenters/AddprojectPresenter.php
+++ app/presenters/AddprojectPresenter.php
@@ -14,23 +14,39 @@ use App\Model\MyDateTime;
 class AddprojectPresenter extends Nette\Application\UI\Presenter {
 
     private $database;
+    private $project = null;
 
     // pro práci s vrstvou Database Explorer si předáme Nette\Database\Context
     public function __construct(Nette\Database\Context $connection, Nette\Database\IStructure $structure, Nette\Database\IConventions $conventions = null, Nette\Caching\IStorage $cacheStorage = null) {
         $this->database = $connection;
     }
 
-    public function renderAdd() {
+    public function renderAdd($projectId = null) {
+        if ($projectId !== null) {
+            $this->project = $this->database->table('projects')->where('idProjects', $projectId)->fetch();
+            $this->template->name = $this->project->Name;
+        }
     }
 
     protected function createComponentAddprojectForm() {
         $form = new Nette\Application\UI\Form;
-        $form->addText('Name')->setRequired('Projekt musí mít název');
-        $form->addTextArea('desc');
-        $form->addSelect('user', null, $this->database->table('users')->where('UserPrivilege', 3)->fetchPairs('idUsers', 'UserName'))->setRequired('Projekt musí mít studenta');
-        $form->addSelect('consultant', null, $this->database->table('users')->where('UserPrivilege', 2)->fetchPairs('idUsers', 'UserName'));
-        $form->addSelect('oponent', null, $this->database->table('users')->where('UserPrivilege', 4)->fetchPairs('idUsers', 'UserName'));
-        $form->addCheckbox('agree');
+        if ($this->project !== null) {
+            $form->addText('Name')->setRequired('Projekt musí mít název')->setValue($this->project->Name);
+            $form->addTextArea('desc')->setValue($this->project->Desc);
+            $form->addSelect('user', null, $this->database->table('users')->where('UserPrivilege', 3)->fetchPairs('idUsers', 'UserName'))->setDefaultValue($this->project->User)->setRequired('Projekt musí mít studenta');
+            $form->addSelect('consultant', null, $this->database->table('users')->where('UserPrivilege', 2)->fetchPairs('idUsers', 'UserName'))->setDefaultValue($this->project->Consultant);
+            $form->addSelect('oponent', null, $this->database->table('users')->where('UserPrivilege', 4)->fetchPairs('idUsers', 'UserName'))->setDefaultValue($this->project->Oponent);
+            $form->addCheckbox('agree')->setValue($this->project->Public);
+            $form->addHidden('preddefined', $this->project->idProjects);
+        } else {
+            $form->addText('Name')->setRequired('Projekt musí mít název');
+            $form->addTextArea('desc');
+            $form->addSelect('user', null, $this->database->table('users')->where('UserPrivilege', 3)->fetchPairs('idUsers', 'UserName'))->setRequired('Projekt musí mít studenta');
+            $form->addSelect('consultant', null, $this->database->table('users')->where('UserPrivilege', 2)->fetchPairs('idUsers', 'UserName'))->setPrompt("");
+            $form->addSelect('oponent', null, $this->database->table('users')->where('UserPrivilege', 4)->fetchPairs('idUsers', 'UserName'))->setPrompt("");
+            $form->addCheckbox('agree');
+            $form->addHidden('preddefined', 0);
+        }
         $form->addSubmit('login');
         $form->onValidate[] = [$this, 'validateProject'];
         $form->onSuccess[] = [$this, 'saveProject'];
@@ -43,9 +59,9 @@ class AddprojectPresenter extends Nette\Application\UI\Presenter {
             $form[desc]->addError('popis přesahuje maximální dovolený limit písmen');
         }
         $res = $this->database->table('projects')
-                ->where('Name',$values->Name)
-                ->where('Year', MyDateTime::getYear(\Nette\Utils\DateTime::from('0')))->count('*');
-        if($res> 0){
+                        ->where('Name', $values->Name)
+                        ->where('Year', MyDateTime::getYear(\Nette\Utils\DateTime::from('0')))->count('*');
+        if ($res > 0) {
             $form['Name']->addError('duplicitni nazev');
             $this->template->form = $form;
         }
@@ -53,20 +69,37 @@ class AddprojectPresenter extends Nette\Application\UI\Presenter {
 
     public function saveProject(UI\Form $form) {
         $values = $form->getValues();
-        try{
-        $projectId = $this->database->table('projects')->insert([
-            'Name' => $values->Name,
-            'FileDir' => 'files',
-            'User' => $values->user,
-            'Consultant' => $values->consultant,
-            'Oponent' => $values->oponent,
-            'Year' => MyDateTime::getYear(\Nette\Utils\DateTime::from('0')),
-            'Public' => ($values->agree == true) ? 1 : 0,
-        ]);
-        } catch (mysqli_sql_exception $e){
-            $this->template->error = var_dump($e);
+        if ($values->preddefined == 0) {
+            try {
+                $projectId = $this->database->table('projects')->insert([
+                    'Name' => $values->Name,
+                    'FileDir' => 'files',
+                    'User' => $values->user,
+                    'Consultant' => $values->consultant,
+                    'Oponent' => $values->oponent,
+                    'Year' => MyDateTime::getYear(\Nette\Utils\DateTime::from('0')),
+                    'Public' => ($values->agree == true) ? 1 : 0,
+                ]);
+            } catch (mysqli_sql_exception $e) {
+                $this->template->error = var_dump($e);
+            }
         }
-        
+        else{
+            try {
+                $projectId = $this->database->table('projects')->where('idProjects',$values->preddefined)->update([
+                    'Name' => $values->Name,
+                    'FileDir' => 'files',
+                    'User' => $values->user,
+                    'Consultant' => $values->consultant,
+                    'Oponent' => $values->oponent,
+                    'Year' => MyDateTime::getYear(\Nette\Utils\DateTime::from('0')),
+                    'Public' => ($values->agree == true) ? 1 : 0,
+                ]);
+            } catch (mysqli_sql_exception $e) {
+                $this->template->error = var_dump($e);
+            }
+        }
+        $this->redirect('Homepage:');
     }
 
 //    public function validateProject(UI\Form $form) {
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,6 +10,7 @@ use Nette\Utils\DateTime;
 class HomepagePresenter extends Nette\Application\UI\Presenter {
 
     private $database;
+    private $project;
 
     public function __construct(Nette\Database\Context $connection, Nette\Database\IStructure $structure, Nette\Database\IConventions $conventions = null, Nette\Caching\IStorage $cacheStorage = null) {
         $this->database = $connection;
@@ -19,8 +20,6 @@ class HomepagePresenter extends Nette\Application\UI\Presenter {
      * show projects
      */
     public function renderDefault() {
-        //show project to defined user
-        $projects;
         if ($this->getUser()->isInRole('administrator')) {
             $projects = $this->database->table('projects')->fetchAll();
         } elseif ($this->getUser()->isInRole('student')) {
@@ -40,11 +39,52 @@ class HomepagePresenter extends Nette\Application\UI\Presenter {
                     ->where(" Year", MyDateTime::getYear(DateTime::from(0)))
                     ->fetchAll();
         }
+        
         $this->template->projects = $projects;
     }
-    public function renderProject($projectId){
-        $project = $this->database->table('projects')->where('idProjects',$projectId)->fetch();
-        $this->template->project = $project;
+
+    public function renderProject($projectId) {
+        $project = $this->database->table('projects')->where('idProjects', $projectId)->fetch();
+        if ($this->user->isInRole('administrator')) {
+            $this->template->project = $project;
+        } else if ($project->Public == 0) {
+            if ($this->user->isLoggedIn() && ($this->user->getIdentity()->username == $project->ref('users', 'User')->UserName ||
+                    $this->user->getIdentity()->username == $project->ref('users', 'Oponent')->UserName ||
+                    $this->user->getIdentity()->username == $project->ref('users', 'Consultant')->UserName)) {
+                $this->template->project = $project;
+            } else {
+                $this->error('Projekt je zamčen veřejnosti');
+            }
+        } else {
+            $this->template->project = $project;
+            $this->project = $project;
+        }
     }
 
+    public function handleDelete($id) {
+        $this->database->table('files')->where('Project', $id)->delete();
+        $this->database->table('projects')->where('idProjects', $id)->delete();
+        $this->flashMessage('Záznam s id ' . $id . 'byl smazán');
+        $this->redirect('Homepage:');
+    }
+    public function handlePubliced($id,$public){
+        $this->database->table('projects')
+                ->where('idProjects', $id)
+                ->update(['Public' => ($public == 1) ? '0' : '1']);
+        if($this->isAjax()){ // jde o ajax pozadavek?
+          $this->redrawControl('Public'); // invalidujes dotycnej snippet
+
+       }else{
+          $this->redirect('this');
+        }
+    }
+    public function handleLocked($id,$locked){
+        $this->database->table('projects')->where('idProjects', $id)->update(['Locked' => ($locked == 1) ? '0' : '1']);
+        if($this->isAjax()){ // jde o ajax pozadavek?
+          $this->redrawControl('Locked'); // invalidujes dotycnej snippet
+
+       }else{
+          $this->redirect('this');
+        }
+    }
 }
